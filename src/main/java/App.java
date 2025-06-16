edb1100172931e663dea218324591b3e41635c9f
vyacheslavkalyakin <kalyakinvvv@mail.ru>
2025-06-16 17:09:23 +0300

    Initial commit from template

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,16 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat one = (Cat) applicationContext.getBean("cat");
+        Cat two = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1 == bean2);
+        System.out.println(one == two);
+
     }
 }
---
+++ src/main/java/Cat.java
@@ -0,0 +1,9 @@
+public class Cat {
+
+    private String name;
+
+    public String getName() { return name;}
+
+    public void setName(String message) { this.name = message;}
+}
+
